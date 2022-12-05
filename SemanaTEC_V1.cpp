712451923567f6e48ccaa1b1a814bbb82ea76b8b
Rafael Ligeski Iung Barbosa <116225787+Rafaleca@users.noreply.github.com>
2022-12-05 20:22:39 -0300

    Rename SemanaTEC_v1.cpp to SemanaTEC_V1.cpp

--- SemanaTEC_V1.cpp
+++ SemanaTEC_V1.cpp
@@ -10,22 +10,22 @@ main () {
 	
 	printf("\n Infome a quantia produzida: ");
 	scanf("%d", &produktion);
-	printf("\n A m·quina est· com defeito: ");
+	printf("\n A m√°quina est√° com defeito: ");
 	scanf("%d", &buchstabe);
 	
-	betriet=produktion*5;//10 minutos, n˙mero ficcional
+	betriet=produktion*5;//10 minutos, n√∫mero ficcional
 	
-	printf("\n O resultado da operaÁ„o foi: %d", betriet);
+	printf("\n O resultado da opera√ß√£o foi: %d", betriet);
 //	if(betriet>=3){
-//		printf("\n A produÁ„o est· em orden");
+//		printf("\n A produ√ß√£o est√° em orden");
 //	}else{
-//		printf("\n A produÁ„o est· com defeito");
+//		printf("\n A produ√ß√£o est√° com defeito");
 //	}
 
 	if(betriet>=200 && buchstabe=="s"){
-		printf("\n A m·quina est· em ordem");
+		printf("\n A m√°quina est√° em ordem");
 	}else{
-		printf("\n A m·quina est· com defeito");
+		printf("\n A m√°quina est√° com defeito");
 	}
 //	
 //	aufteilung=betriet/3;
@@ -37,7 +37,7 @@ main () {
 
   Este projeto nao foi finalizado, por isso que aparenta esta inacabado.
   
-  A ideia desse programa era de mostrar dados, cinco dados se n„o me engano e
+  A ideia desse programa era de mostrar dados, cinco dados se n√£o me engano e
   desse resultado um dos cinco resultados estaria fora do normal, assim
   indicando que a maquina estaria quebrada, assim auxiliando na explicacao.
 
